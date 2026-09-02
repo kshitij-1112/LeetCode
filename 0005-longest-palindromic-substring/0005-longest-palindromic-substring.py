8bca9a7edbd917808733d53a09137e7071c317f4
Kshitij Tiwari <142715182+kshitij-1112@users.noreply.github.com>
2026-09-02 22:37:06 +0530

    Time: 222 ms (89.13%), Space: 19.3 MB (69.58%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,40 +1,30 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        """
-        Find the longest palindromic substring using dynamic programming.
-      
-        Args:
-            s: Input string
-          
-        Returns:
-            The longest palindromic substring
-        """
-        n = len(s)
-      
-        # dp[i][j] represents whether substring s[i:j+1] is a palindrome
-        # Initialize all single characters as palindromes (True)
-        dp = [[True] * n for _ in range(n)]
-      
-        # Variables to track the starting position and length of longest palindrome
-        start_pos = 0
-        max_length = 1
-      
-        # Fill the dp table bottom-up (from longer indices to shorter)
-        for i in range(n - 2, -1, -1):
-            for j in range(i + 1, n):
-                # Initially mark as False for substrings with length > 1
-                dp[i][j] = False
-              
-                # Check if characters at both ends match
-                if s[i] == s[j]:
-                    # For substrings of length 2: just check if ends match
-                    # For longer substrings: also check if inner substring is palindrome
-                    dp[i][j] = dp[i + 1][j - 1]
-                  
-                    # Update longest palindrome if current one is longer
-                    if dp[i][j] and max_length < j - i + 1:
-                        start_pos = i
-                        max_length = j - i + 1
-      
-        # Return the longest palindromic substring
-        return s[start_pos : start_pos + max_length]
+  def longestPalindrome(self, s: str) -> str:
+    if not s:
+      return ''
+
+    # (start, end) indices of the longest palindrome in s
+    indices = [0, 0]
+
+    def extend(s: str, i: int, j: int) -> tuple[int, int]:
+      """
+      Returns the (start, end) indices of the longest palindrome extended from
+      the substring s[i..j].
+      """
+      while i >= 0 and j < len(s):
+        if s[i] != s[j]:
+          break
+        i -= 1
+        j += 1
+      return i + 1, j - 1
+
+    for i in range(len(s)):
+      l1, r1 = extend(s, i, i)
+      if r1 - l1 > indices[1] - indices[0]:
+        indices = l1, r1
+      if i + 1 < len(s) and s[i] == s[i + 1]:
+        l2, r2 = extend(s, i, i + 1)
+        if r2 - l2 > indices[1] - indices[0]:
+          indices = l2, r2
+
+    return s[indices[0]:indices[1] + 1]
