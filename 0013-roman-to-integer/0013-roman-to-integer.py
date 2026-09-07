365fc2aadfee7f993f523966e6fd097fb9b34bc2
Kshitij Tiwari <142715182+kshitij-1112@users.noreply.github.com>
2026-09-07 17:21:27 +0530

    Time: 7 ms (41.25%), Space: 19.4 MB (20.23%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,21 +1,13 @@
 class Solution:
-    def romanToInt(self, s: str) -> int:
-        values = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
+  def romanToInt(self, s: str) -> int:
+    ans = 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+             'C': 100, 'D': 500, 'M': 1000}
 
-        ans = 0
+    for a, b in zip(s, s[1:]):
+      if roman[a] < roman[b]:
+        ans -= roman[a]
+      else:
+        ans += roman[a]
 
-        for i in range(len(s)):
-            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
-                ans -= values[s[i]]
-            else:
-                ans += values[s[i]]
-
-        return ans
+    return ans + roman[s[-1]]
