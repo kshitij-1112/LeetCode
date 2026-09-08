c87e7f992de19e864cfda48b5820ae060db33731
Kshitij Tiwari <142715182+kshitij-1112@users.noreply.github.com>
2026-09-08 21:18:36 +0530

    Time: 445 ms (96.71%), Space: 22.3 MB (32.21%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,37 +1,37 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
-        res = []
+        n = len(nums)
+        ans = []
 
-        for i in range(len(nums) - 2):
-            # Skip duplicate first values
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
+        for i in range(n - 2):
+            a = nums[i]
 
-            # No solution possible from here
-            if nums[i] > 0:
+            if a > 0:
                 break
 
-            left, right = i + 1, len(nums) - 1
+            if i and a == nums[i - 1]:
+                continue
 
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
+            l, r = i + 1, n - 1
+
+            while l < r:
+                total = a + nums[l] + nums[r]
 
                 if total < 0:
-                    left += 1
+                    l += 1
                 elif total > 0:
-                    right -= 1
+                    r -= 1
                 else:
-                    res.append([nums[i], nums[left], nums[right]])
+                    ans.append([a, nums[l], nums[r]])
 
-                    left += 1
-                    right -= 1
+                    left_val = nums[l]
+                    right_val = nums[r]
 
-                    # Skip duplicate second/third values
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
+                    while l < r and nums[l] == left_val:
+                        l += 1
 
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
+                    while l < r and nums[r] == right_val:
+                        r -= 1
 
-        return res
+        return ans
