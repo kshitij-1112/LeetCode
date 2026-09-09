65642fc045e2470996fe2eed74fa6d670d5dc5de
Kshitij Tiwari <142715182+kshitij-1112@users.noreply.github.com>
2026-09-09 22:05:06 +0530

    Time: 385 ms (50.31%), Space: 19.3 MB (46.15%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,25 +1,27 @@
 class Solution:
-  def threeSumClosest(self, nums: list[int], target: int) -> int:
-    ans = nums[0] + nums[1] + nums[2]
+    def threeSumClosest(self, nums: list[int], target: int) -> int:
+        nums.sort()
+        n = len(nums)
+        closest = nums[0] + nums[1] + nums[2]
 
-    nums.sort()
+        for i in range(n - 2):
+            # Skip duplicate first elements.
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
 
-    for i in range(len(nums) - 2):
-      if i > 0 and nums[i] == nums[i - 1]:
-        continue
-      # Choose nums[i] as the first number in the triplet, then search the
-      # remaining numbers in [i + 1, n - 1].
-      l = i + 1
-      r = len(nums) - 1
-      while l < r:
-        summ = nums[i] + nums[l] + nums[r]
-        if summ == target:
-          return summ
-        if abs(summ - target) < abs(ans - target):
-          ans = summ
-        if summ < target:
-          l += 1
-        else:
-          r -= 1
+            left, right = i + 1, n - 1
 
-    return ans
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if abs(total - target) < abs(closest - target):
+                    closest = total
+
+                if total < target:
+                    left += 1
+                elif total > target:
+                    right -= 1
+                else:
+                    return target
+
+        return closest
