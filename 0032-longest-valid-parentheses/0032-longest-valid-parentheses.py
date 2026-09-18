d399983bab9bd8ea24d957f21e62b05ad09923ef
Kshitij Tiwari <142715182+kshitij-1112@users.noreply.github.com>
2026-09-18 22:18:36 +0530

    Time: 3 ms (99.62%), Space: 19.4 MB (95.03%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -1,17 +1,19 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        stack = [-1]
+        n = len(s)
+        if n < 2:
+            return 0
+            
+        dp = [0] * n
         max_len = 0
         
-        for i, char in enumerate(s):
-            if char == '(':
-                stack.append(i)
-            else:
-                stack.pop()
-                if not stack:
-                    # Base/sentinel for the next valid substring
-                    stack.append(i)
-                else:
-                    max_len = max(max_len, i - stack[-1])
-                    
+        for i in range(1, n):
+            if s[i] == ')':
+                if s[i - 1] == '(':
+                    dp[i] = (dp[i - 2] if i >= 2 else 0) + 2
+                elif i - dp[i - 1] - 1 >= 0 and s[i - dp[i - 1] - 1] == '(':
+                    dp[i] = dp[i - 1] + (dp[i - dp[i - 1] - 2] if i - dp[i - 1] - 2 >= 0 else 0) + 2
+                
+                max_len = max(max_len, dp[i])
+                
         return max_len
