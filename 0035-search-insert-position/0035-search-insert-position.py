b22feefad2fa05159a57f9c60c5e6d5acb792c5f
Kshitij Tiwari <142715182+kshitij-1112@users.noreply.github.com>
2026-09-19 22:13:43 +0530

    Time: 0 ms (100%), Space: 19.9 MB (82.15%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,6 +1,14 @@
-from bisect import bisect_left
 from typing import List
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        return bisect_left(nums, target)
+        left, right = 0, len(nums)
+        
+        while left < right:
+            mid = (left + right) >> 1  # Bitwise shift for fast division by 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+                
+        return left
