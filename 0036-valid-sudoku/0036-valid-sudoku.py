b2e8df6b9d624f79b677a45a566d29510540f16b
Kshitij Tiwari <142715182+kshitij-1112@users.noreply.github.com>
2026-09-20 22:27:55 +0530

    Time: 3 ms (74.67%), Space: 19.3 MB (75.33%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,18 +1,25 @@
 class Solution:
+
   def isValidSudoku(self, board: list[list[str]]) -> bool:
     seen = set()
 
-    for i in range(9):
-      for j in range(9):
-        c = board[i][j]
-        if c == '.':
+    for r in range(9):
+      for c in range(9):
+        val = board[r][c]
+        if val == ".":
           continue
-        if (c + '@row ' + str(i) in seen or
-            c + '@col ' + str(j) in seen or
-                c + '@box ' + str(i // 3) + str(j // 3) in seen):
+
+        # Create unique string identifiers for row, column, and 3x3 sub-box
+        row_id = f"row {r}: {val}"
+        col_id = f"col {c}: {val}"
+        box_id = f"box {r // 3},{c // 3}: {val}"
+
+        # If any identifier already exists, the board is invalid
+        if row_id in seen or col_id in seen or box_id in seen:
           return False
-        seen.add(c + '@row ' + str(i))
-        seen.add(c + '@col ' + str(j))
-        seen.add(c + '@box ' + str(i // 3) + str(j // 3))
+
+        seen.add(row_id)
+        seen.add(col_id)
+        seen.add(box_id)
 
     return True
